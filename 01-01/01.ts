ce999e2b2821497cfebce98ba9e192b2c23d0106
Stefan Temme <temme@objectcode.de>
2024-12-01 11:41:01 +0100

    Star 2

--- 01-01/01.ts
+++ 01-01/01.ts
@@ -8,14 +8,30 @@ const parseData = (data: string) => {
         right.push(r);
         return [left, right]
     }, [[], []])
-    left.sort((a,b)=>Math.sign(a-b))
-    right.sort((a,b)=>Math.sign(a-b))
-    return {left,right};
+    left.sort((a, b) => Math.sign(a - b))
+    right.sort((a, b) => Math.sign(a - b))
+    return {left, right};
+}
+
+const bag = (list: number[]) => {
+    const bag = new Map<number, number>();
+    list.forEach(i => {
+        if (!bag.has(i)) {
+            bag.set(i, 0);
+        }
+        bag.set(i, bag.get(i) + 1);
+    })
+    return bag;
 }
 const solve = (data: string) => {
-    const {left,right}=parseData(data);
-    const distance = left.reduce((sum,l,i)=>sum+Math.abs(l-right[i]),0)
-    console.log(distance);
+    const {left, right} = parseData(data);
+    const distance = left.reduce((sum, l, i) => sum + Math.abs(l - right[i]), 0)
+    console.log("total distance", distance);
+    const rightBag = bag(right);
+    const score = left.reduce((sum, l) => sum + l * (rightBag.get(l) ?? 0), 0)
+
+    console.log("similarity score", score)
+
 }
 console.time("Example");
 console.log("Example")
