ae4d4e02cd7824a24e7c048678cbf0c39bed93f7
Stefan Temme <temme@objectcode.de>
2025-01-08 14:37:22 +0100

    day 15 star 2

--- 09-15/15.ts
+++ 09-15/15.ts
@@ -15,81 +15,116 @@ const parse = (data: string) => {
     const [mapData, operationData] = data.split("\n\n");
 
     let robot: TCell;
+    const boxes = new Map<TCell, TCell[]>();
     const map: TCell[][] = mapData.split("\n").map((row, i) => row.split("").map((v, j) => {
         const isBox = v == box;
         const isWall = v == wall;
         if (v == robotText) {
             robot = {i, j, isBox, isWall}
         }
-        return {i, j, isBox, isWall}
+        const cell = {i, j, isBox, isWall};
+        if (isBox) {
+            boxes.set(cell, [cell]);
+        }
+        return cell;
     }))
 
     const operations = operationData.split("\n").map(r => r.split("") as (keyof typeof dirs)[]).flat();
-    return {robot, map, operations}
+    return {robot, map, operations, boxes}
 }
-
-const print = (map: TCell[][], robot: TCell) => {
-    const letters = map.map(r => r.map(c => c.isWall ? wall : c.isBox ? box : c.i == robot.i && c.j == robot.j ? robotText : ".").join("")).join("\n")
-    console.log(letters);
-}
-const step = (map: TCell[][], robot: TCell, operation: keyof typeof dirs) => {
-    const dir = dirs[operation];
-    let ahead = map[robot.i + dir[0]][robot.j + dir[1]];
-    while (ahead.isBox) {
-        ahead = map[ahead.i + dir[0]][ahead.j + dir[1]];
-    }
-    if (!ahead.isWall) {
-        //free
-        while (ahead.i != robot.i || ahead.j != robot.j) {
-            const back = map[ahead.i - dir[0]][ahead.j - dir[1]];
-            ahead.isBox = back.isBox;
-            ahead = back;
-        }
-        robot.i += dir[0];
-        robot.j += dir[1];
-    }
-}
-
-const solve = (data: string) => {
-    const {map, robot, operations} = parse(data);
-    // print(map,robot);
-    for (const operation of operations) {
-        // console.log(operation)
-        step(map, robot, operation);
-        // print(map,robot)
-    }
-    const score = map.flat().filter(c => c.isBox).map(c => c.i * 100 + c.j).reduce((a, b) => a + b, 0);
-    console.log("GPS Score", score);
-}
-
 const parse2 = (data: string) => {
     const [mapData, operationData] = data.split("\n\n");
 
     let robot: TCell;
-    const boxes = new Map<TCell,TCell[]>();
+    const boxes = new Map<TCell, TCell[]>();
     const map: TCell[][] = mapData.split("\n").map((row, i) => row.split("").map((v, j) => {
             const isBox = v == box;
             const isWall = v == wall;
             if (v == robotText) {
-                robot = {i: 2 * i, j, isBox, isWall}
+                robot = {i, j: 2 * j, isBox, isWall}
             }
             const cells = [
-                {i: 2 * i, j, isBox, isWall},
-                {i: 2 * i + 1, j, isBox, isWall}
+                {i, j: 2 * j, isBox, isWall},
+                {i, j: 2 * j + 1, isBox, isWall}
             ];
-            if (isBox){
-                boxes
+            if (isBox) {
+                cells.forEach(c => {
+                    boxes.set(c, cells);
+                });
             }
             return cells;
         }).flat()
     )
 
     const operations = operationData.split("\n").map(r => r.split("") as (keyof typeof dirs)[]).flat();
-    return {robot, map, operations}
+    return {robot, map, operations, boxes}
 };
-const solve2 = (data: string) => {
-    const parsed = parse2(data);
+const print = (map: TCell[][], robot: TCell, doubled = false) => {
+    let i = 0
+    const b = () => doubled ? ["[", "]"][i++ % 2] : box;
+    const letters = map.map(r => r.map(c => c.isWall ? wall : c.isBox ? b() : c.i == robot.i && c.j == robot.j ? robotText : ".").join("")).join("\n")
+    console.log(letters);
 }
+
+const step2 = (map: TCell[][], robot: TCell, operation: keyof typeof dirs, boxes: Map<TCell, TCell[]>) => {
+    const dir = dirs[operation];
+    let movedBoxes = new Set<TCell[]>();
+    const checkAhead = (ahead: TCell[]) => {
+        let boxesAhead = ahead.filter(s => s.isBox);
+        let wallsAhead = ahead.filter(s => s.isWall);
+        return {boxesAhead, wallsAhead}
+    }
+    const getNext = (cell: TCell) => map[cell.i + dir[0]][cell.j + dir[1]];
+    let {boxesAhead, wallsAhead} = checkAhead([getNext(robot)]);
+    while (boxesAhead.length > 0 && wallsAhead.length == 0) {
+        const boxcellsToBoxes = boxesAhead.map(b => boxes.get(b));
+        boxcellsToBoxes.forEach(b => movedBoxes.add(b));
+        const allBoxcells = new Set(boxcellsToBoxes.flat());
+        const ahead = [...allBoxcells].map(getNext).filter(c => !allBoxcells.has(c));
+        const check = checkAhead(ahead);
+        boxesAhead = check.boxesAhead;
+        wallsAhead = check.wallsAhead;
+    }
+    if (wallsAhead.length == 0) {
+        //free
+        for (const b of [...movedBoxes].reverse()) {
+            const f = b.map(getNext);
+            b.forEach(b1 => {
+                b1.isBox = false;
+                boxes.delete(b1);
+            });
+            f.forEach(f1 => {
+                f1.isBox = true;
+                boxes.set(f1, f);
+            });
+        }
+        robot.i += dir[0];
+        robot.j += dir[1];
+    }
+}
+
+const solve = (data: string, secondPass = false) => {
+    const {map, robot, operations, boxes} = parse(data);
+    const score = computeScore(operations, map, robot, boxes);
+    console.log("GPS Score I", score);
+    if (secondPass) {
+        const {map, robot, operations, boxes} = parse2(data);
+        const score = computeScore(operations, map, robot, boxes);
+        console.log("GPS Score II", score);
+    }
+}
+
+const computeScore = (operations: (keyof typeof dirs)[], map: TCell[][], robot: TCell, boxes: Map<TCell, TCell[]>) => {
+    // print(map, robot, true);
+    for (const operation of operations) {
+        // console.log(operation)
+        step2(map, robot, operation, boxes);
+        // print(map,robot,true)
+    }
+    const score = [...new Set(boxes.values())].map(c => c.reduce((a, b) => Math.min(a, b.i), Number.MAX_VALUE) * 100 + c[0].j).reduce((a, b) => a + b, 0);
+    return score;
+}
+
 console.log("Advent of Code - 2024 - 15")
 console.log("https://adventofcode.com/2024/day/15")
 console.time("Example small");
@@ -99,9 +134,9 @@ console.timeEnd("Example small")
 
 console.time("Example");
 console.log("Example (10092)")
-solve(exampleData);
+solve(exampleData, true);
 console.timeEnd("Example")
 console.time("Stardata");
 console.log("Stardata")
-solve(starData);
+solve(starData, true);
 console.timeEnd("Stardata");
